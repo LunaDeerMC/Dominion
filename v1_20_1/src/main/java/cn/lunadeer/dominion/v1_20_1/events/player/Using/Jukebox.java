59dc4e06e0b0d9c50c1b8d1994ba8bea091d62ae
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-08-15 10:42:08 +0800

    feat #127: add interaction flag and event handler for jukebox

---
+++ v1_20_1/src/main/java/cn/lunadeer/dominion/v1_20_1/events/player/Using/Jukebox.java
@@ -0,0 +1,21 @@
+package cn.lunadeer.dominion.v1_20_1.events.player.Using;
+
+import cn.lunadeer.dominion.api.dtos.flag.Flags;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import static cn.lunadeer.dominion.misc.Others.checkPrivilegeFlag;
+
+public class Jukebox implements Listener {
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void handler(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.getClickedBlock() == null) return;
+        if (event.getClickedBlock().getType() != Material.JUKEBOX) return;
+        checkPrivilegeFlag(event.getClickedBlock().getLocation(), Flags.JUKEBOX, event.getPlayer(), event);
+    }
+}
