1ffc617b64e93ff5114137057753a3a302c825b7
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-09-02 22:51:23 +0800

    fix: update Liquid event handler to use PlayerBucketEmptyEvent and adjust version to 4.6.2

--- v1_20_1/src/main/java/cn/lunadeer/dominion/v1_20_1/events/player/Place/Liquid.java
+++ v1_20_1/src/main/java/cn/lunadeer/dominion/v1_20_1/events/player/Place/Liquid.java
@@ -1,34 +1,19 @@
 package cn.lunadeer.dominion.v1_20_1.events.player.Place;
 
 import cn.lunadeer.dominion.api.dtos.flag.Flags;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
 
 import static cn.lunadeer.dominion.misc.Others.checkPrivilegeFlag;
 
 public class Liquid implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
-    public void handler(PlayerInteractEvent event) {
+    public void handler(PlayerBucketEmptyEvent event) {
         if (event.isCancelled()) return;
-        if (event.getItem() == null) {
-            return;
-        }
-        if (event.getItem().getType() != Material.LAVA_BUCKET &&
-                event.getItem().getType() != Material.WATER_BUCKET) {
-            return;
-        }
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
-            return;
-        }
-        if (event.getClickedBlock() == null) {
-            return;
-        }
         Player player = event.getPlayer();
-        checkPrivilegeFlag(event.getClickedBlock().getLocation(), Flags.PLACE, player, event);
+        checkPrivilegeFlag(event.getBlockClicked().getLocation(), Flags.PLACE, player, event);
     }
 }
