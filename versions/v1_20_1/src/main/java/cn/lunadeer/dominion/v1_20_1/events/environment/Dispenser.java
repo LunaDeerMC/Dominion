4dfa9ae04a984ca2b83d43753decbbeb9286f234
zhangyuheng <zhangyuheng@lunadeer.cn>
2026-02-15 23:40:59 +0800

    feat(event): add dispenser event listener to prevent item dispensing into dominions

---
+++ versions/v1_20_1/src/main/java/cn/lunadeer/dominion/v1_20_1/events/environment/Dispenser.java
@@ -0,0 +1,28 @@
+package cn.lunadeer.dominion.v1_20_1.events.environment;
+
+import cn.lunadeer.dominion.api.dtos.DominionDTO;
+import cn.lunadeer.dominion.cache.CacheManager;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Directional;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
+
+public class Dispenser implements Listener {
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void handler(BlockDispenseEvent event) {
+        if (event.isCancelled()) return;
+        if (!event.getBlock().getType().equals(Material.DISPENSER)) return;
+        BlockFace blockFace = null;
+        if (event.getBlock().getBlockData() instanceof Directional directional) {
+            blockFace = directional.getFacing();
+        }
+        if (blockFace == null) return;
+        DominionDTO dominion = CacheManager.instance.getDominion(event.getBlock().getRelative(blockFace).getLocation());
+        if (dominion == null) return;
+        event.setCancelled(true);   // stop the dispenser from dispensing items into the dominion
+    }
+
+}
